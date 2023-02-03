059e5386bc3134280094a4ca27c451b3e6cdfb47
dmdbs <bloodyshark1337@gmail.com>
2023-02-03 20:56:44 +0500

    Update preview.blade.php

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -1,5 +1,7 @@
-<!DOCTYPE html>
-<html lang="ru">
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office" lang="ru">
 <head>
     <title>Generate PDF Laravel 8 - phpcodingstuff.com</title>
     <meta charset="UTF-8">
