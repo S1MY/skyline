9ea07138bd2973b21c99c07e2a5f6806e4a3f431
S1MY <71487094+S1MY@users.noreply.github.com>
2023-02-03 16:44:37 +0500

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'mail.myskylinecompany.com'),
+            'port' => env('MAIL_PORT', 995),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('info@myskylinecompany.com'),
+            'password' => env('uM2mI2dY0yiZ3cH9'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
