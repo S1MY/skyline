ea88b2e018b7f77936446a202b9a3f46dfc48b97
S1MY <71487094+S1MY@users.noreply.github.com>
2023-02-03 19:47:44 +0500

    PDF

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use PDF;
+
+class PDFController extends Controller
+{
+    public function preview()
+    {
+        return view('preview');
+    }
+
+    public function generatePDF()
+    {
+        $pdf = PDF::loadView('preview');
+        return $pdf->download('demo.pdf');
+    }
+}
