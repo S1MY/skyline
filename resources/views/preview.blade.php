980f7f3d59d29225a11efa55985053b611a9d971
dmdbs <bloodyshark1337@gmail.com>
2023-02-03 21:26:30 +0500

    Update preview.blade.php

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -14,7 +14,7 @@
     @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap');
     * {
         box-sizing: border-box;
-        font-family: 'Roboto',sans-serif, Arial, Helvetica !important;
+        font-family: 'DejaVu Sans' !important;
     }    
 html{margin:40px 50px}
     .customTable{
@@ -73,6 +73,7 @@ table {
     margin: 0 auto;
     border-collapse: separate;
     border-spacing: 0 12px;
+    border: none;
 		}
 		th {
 			padding:16px;
