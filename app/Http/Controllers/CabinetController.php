7cddbe724c5f45bf2333185325a0fc22e0c73aff
S1MY <71487094+S1MY@users.noreply.github.com>
2023-02-03 19:59:37 +0500

    Update CabinetController.php

--- app/Http/Controllers/CabinetController.php
+++ app/Http/Controllers/CabinetController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
-use Barryvdh\DomPDF\Facade\PDF;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class CabinetController extends Controller
 {
