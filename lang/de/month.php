0cde4c2e7578b194dd877248ee8eefd91f66b618
S1MY <71487094+S1MY@users.noreply.github.com>
2023-02-06 15:23:04 +0500

    translate months

---
+++ lang/de/month.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'jun' => 'Januars',
+    'feb' => 'Februar',
+    'mar' => 'Maerz',
+    'apr' => 'April',
+    'may' => 'Mai',
+    'jun' => 'Juni',
+    'jul' => 'Julis',
+    'aug' => 'August',
+    'sep' => 'September',
+    'oct' => 'Oktober',
+    'nov' => 'November',
+    'dec' => 'Dezember',
+];
---
+++ lang/en/month.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'jun' => 'January',
+    'feb' => 'February',
+    'mar' => 'March',
+    'apr' => 'April',
+    'may' => 'May',
+    'jun' => 'June`s',
+    'jul' => 'July`s',
+    'aug' => 'August',
+    'sep' => 'September',
+    'oct' => 'October`s',
+    'nov' => 'November`s',
+    'dec' => 'december',
+];
