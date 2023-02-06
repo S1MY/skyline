628fe35c92d22a6c6c6597363cd55c4c31ced09b
S1MY <71487094+S1MY@users.noreply.github.com>
2023-02-06 18:29:17 +0500

    Update CabinetController.php

--- app/Http/Controllers/CabinetController.php
+++ app/Http/Controllers/CabinetController.php
@@ -746,6 +746,6 @@ class CabinetController extends Controller
         $pdf->save($path . '/' . $fileName);
 
         $pdf = public_path('pdf/'.$fileName);
-        return response()->download($pdf);
+        return $pdf;
     }
 }
