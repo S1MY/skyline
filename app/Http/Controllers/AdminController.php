b550d3cfeb2c6c22eed277ac47f7a86eff9a959b
S1MY <71487094+S1MY@users.noreply.github.com>
2023-02-09 12:19:18 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -84,8 +84,8 @@ class AdminController extends Controller
             ->orWhere('user_id', '=', $user)
             ->first();
         }else{
-            $userInfo = UserInfo::where('user_show_id', '=', $user)
-            ->select('name', 'surname', 'user_infos.user_show_id', 'user_infos.avatar')
+            $userInfo = UserInfo::select('name', 'surname', 'user_infos.user_show_id', 'user_infos.avatar')
+            ->where('user_show_id', '=', $user)
             ->orWhere('user_id', '=', $user)
             ->first();
 
